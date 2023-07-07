7a397dad439e24b98ccd5951e725d1a2f506c1cb
doraeza <pp8960@naver.com>
2023-07-07 17:43:22 +0900

    cookie

--- js-basic/src/hooks/useCookie/index.tsx
+++ js-basic/src/hooks/useCookie/index.tsx
@@ -1,4 +1,17 @@
+import { Cookies } from "react-cookie"
+
 const useCookie = () => {
 
+    const cookies = new Cookies()
+
+    const setCookie = (name:string, value: string, option?: any) => {
+        return cookies.set(name, value, {...option})
+    }
+
+    const getCookie = (name: string) => {
+        return cookies.get(name)
+    }
+
+    return {setCookie, getCookie}
 }
 export default useCookie
