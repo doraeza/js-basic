a35b00539665736dd09c942bd8098cfbe9cbfe98
doraeza <pp8960@naver.com>
2023-07-08 22:45:39 +0900

    jwt2

--- js-basic/src/hooks/useJwt/index.tsx
+++ js-basic/src/hooks/useJwt/index.tsx
@@ -1,4 +1,11 @@
+import jwt from 'jsonwebtoken'
+import fs from 'fs'
 const useJwt = () => {
-
+    const privateKey = fs.readFileSync("private.key");
+    const token = jwt.sign({ email: "test@user.com" }, privateKey, {
+      algorithm: "RS256",
+      expiresIn: "1h"
+    });
+    console.log(token)
 }
 export default useJwt
